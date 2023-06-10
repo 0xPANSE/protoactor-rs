3d5f61c4ea643aa298dcabdcc5bb6b549d48f8ab
Milan Jaric <327155+mjaric@users.noreply.github.com>
2023-06-10 11:42:37 +0200

    refactoring green_threads sample

--- samples/src/green_threads.rs
+++ samples/src/green_threads.rs
@@ -1,3 +1,6 @@
+use log::{info, warn};
+use std::time::Instant;
+
 #[derive(Debug)]
 enum Msg {
     Ping,
@@ -6,6 +9,11 @@ enum Msg {
 }
 
 fn main() {
+    env_logger::builder()
+        .target(env_logger::Target::Stdout)
+        .filter_level(log::LevelFilter::Debug)
+        .format_timestamp_micros()
+        .init();
     tokio::runtime::Builder::new_multi_thread()
         .worker_threads(10)
         .enable_all()
@@ -19,52 +27,82 @@ fn main() {
             let tx21 = tx2.clone();
             let ping = tokio::spawn(async move {
                 let mut count = 0;
+                let mut timer: Option<Instant> = None;
                 loop {
                     match rx1.recv().await {
                         Some(Msg::Ping) => {
+                            if let Some(start) = timer {
+                                if start.elapsed().as_secs() >= 1 {
+                                    info!( target: "ping", "elapsed: {:?}, counted: {:?}",
+                                        start.elapsed(),
+                                        count
+                                    );
+                                    timer = None;
+                                }
+                            }
+                            if tx2.is_closed() {
+                                info!("tx2 is closed");
+                                break;
+                            }
                             tx2.send(Msg::Pong).unwrap();
                         }
                         Some(Msg::Report) => {
-                            println!("Ping count: {}", count);
-                            break;
+                            info!("ping: {:?}", count);
+                            timer = Some(Instant::now());
+                            count = 0;
                         }
                         _ => {
-                            println!("rx1.recv() returned None");
+                            warn!(target: "ping", "rx1.recv() returned None");
                             break;
                         }
                     }
                     count += 1;
-                    tx2.send(Msg::Pong).unwrap();
                 }
             });
 
             let pong = tokio::spawn(async move {
                 let mut count = 0;
+                let mut started: Option<Instant> = None;
                 loop {
                     match rx2.recv().await {
                         Some(Msg::Pong) => {
+                            if let Some(start) = started {
+                                if start.elapsed().as_secs() >= 1 {
+                                    info!( target: "pong",
+                                        "elapsed: {:?}, counted: {:?}",
+                                        start.elapsed(),
+                                        count
+                                    );
+                                    started = None;
+                                }
+                            }
+                            if tx1.is_closed() {
+                                warn!(target: "pong", "tx1 is closed");
+                                break;
+                            }
                             tx1.send(Msg::Ping).unwrap();
                         }
                         Some(Msg::Report) => {
-                            println!("Ping count: {}", count);
-                            break;
+                            info!("pong: {:?}", count);
+                            started = Some(Instant::now());
+                            count = 0;
                         }
                         _ => {
-                            println!("rx2.recv() returned None");
+                            warn!(target: "pong", "rx2.recv() returned None");
                             break;
                         }
                     }
                     count += 1;
-                    tx1.send(Msg::Ping).unwrap();
                 }
             });
             tx11.send(Msg::Ping).unwrap();
+            // warmup
             tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-            tx21.send(Msg::Report).unwrap();
+            // reset counters and report in 1 second
             tx11.send(Msg::Report).unwrap();
+            tx21.send(Msg::Report).unwrap();
             tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-
-            let j = tokio::join!(ping, pong);
-            println!("j: {:?}", j);
+            ping.abort();
+            pong.abort();
         });
 }
